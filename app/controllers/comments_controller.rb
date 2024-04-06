b042ba545803da48874fea7a6f3f4dfc42404823
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-04-06 23:11:31 +0000

    開発準備

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def new
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,14 @@
 class PlacesController < ApplicationController
+
+  def new
+
+  end
+
+  def index
+
+  end
+
+  def show
+
+  end
 end
