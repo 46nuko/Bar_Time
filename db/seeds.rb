4bf34a88ff92d8a701c8936c7abed367e6f15825
EC2 Default User <152859182+46nuko@users.noreply.github.com>
2024-06-08 14:28:37 +0000

    データ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-Admin = Admin.find_or_create_by!(email: ENV['ADMIN_EMAIL']) do |admin|
-  admin.password = {ENV['ADMIN_PASSWORD']}
+Admin = Admin.find_or_create_by!(email: "admin@example.com") do |admin|
+  user.password = "password"
 end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
